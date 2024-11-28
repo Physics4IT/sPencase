9d7c16f7cd08576050ef9567d864c7d6a6663964
LancelotMoretti <123616269+LancelotMoretti@users.noreply.github.com>
2024-11-28 13:22:17 +0700

    Add register screen

--- client/src/components/register/RegisterPage.tsx
+++ client/src/components/register/RegisterPage.tsx
@@ -4,9 +4,41 @@ import "./registerPage.css"
 
 import bg_img from "../../assets/img/bg_img.jpg"
 import web_logo from "../../assets/img/logo.png"
+import { Card, CardContent, CardHeader } from "../ui/card";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useState } from "react";
 
 function RegisterPage() {
     const nav = useNavigate()
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleRegister = async () => {
+        const data = {
+            username: username,
+            email: email,
+            password: password,
+            phonenum: ""
+        }
+
+        const response = await fetch("http://localhost:5000/api/users/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+
+        if (response.ok) {
+            const res = await response.json()
+            console.log(res)
+            nav("/login")
+        } else {
+            console.log("Error")
+        }
+    }
 
     return (
         <div className="overflow-hidden flex relative">
@@ -16,7 +48,36 @@ function RegisterPage() {
                     <img src={web_logo} alt="" className="web-logo z-30" onClick={() => nav("/")}/>
                 </div>
 
-                <div className="section h-auto mt-4 flex-col justify-center items-center">
+                <div className="section h-[72dvh] mt-4 flex-col justify-center items-center">
+                    <Card className="card h-full mr-8">
+                        <CardHeader className="card-container-register-center h-[20%]">
+                            <p className="card-header">Đăng ký tài khoản</p>
+                        </CardHeader>
+                        <CardContent className="card-container-register-start h-[80%]">
+                            <p className="info-name-register">Tên đăng nhập</p>
+                            <Input className="info-input-register" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="sPencase"/>
+                            <p className="info-name-register">Email</p>
+                            <Input className="info-input-register" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="example@gmail.com"/>
+                            <p className="info-name-register">Mật khẩu</p>
+                            <Input className="info-input-register" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="••••••••••" type="password"/>
+
+                            <div className="flex flex-row justify-between">
+                                <div className="flex flex-row justify-start items-center">
+                                    <p className="text-xl text-white font-thin mr-2">Hoặc</p>
+                                    <p
+                                        className="text-xl text-lime-500 font-bold ml-2 underline decoration-lime-500 cursor-pointer hover:text-lime-600 hover:decoration-lime-600"
+                                        onClick={() => nav("/login")}
+                                    >Đăng nhập</p>
+                                </div>
+                                <Button
+                                    className="btn-register"
+                                    onClick={() => handleRegister()}
+                                >Đăng ký</Button>
+                            </div>
+                        </CardContent>
+
+
+                    </Card>
                 </div>
             </div>
         </div>
